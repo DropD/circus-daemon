766c8f89f6f8f89fc67baf74001924566e244a06
DropD <r.haeuselmann@gmx.ch>
2018-02-23 13:22:33 +0100

    balrog ready for transfer into workflows

--- balrog.py
+++ balrog.py
@@ -8,90 +8,92 @@ except ImportError:
 
 import click
 from circus import logger, get_arbiter
+from circus.circusd import get_maxfd, daemonize, closerange
 from circus.arbiter import Arbiter
 from circus.pidfile import Pidfile
 from circus import __version__
-from circus.util import MAXFD, REDIRECT_TO, configure_logger, LOG_LEVELS
+from circus.util import configure_logger
 from circus.util import check_future_exception_and_log
+from circus.client import CircusClient
+from circus.exc import CallError
 from aiida import load_dbenv
 load_dbenv()
-from aiida.settings import get_config, get_default_profile
+from aiida.common.setup import get_config, get_default_profile, CIRCUS_PORT_KEY, generate_new_circus_port, update_profile
 
 
 DEFAULT_PROFILE = get_default_profile('verdi')
-
-
-def get_maxfd():
-    if not resource:
-        maxfd = MAXFD
-    else:
-        maxfd = resource.getrlimit(resource.RLIMIT_NOFILE)[1]
-        if maxfd == resource.RLIM_INFINITY:
-            maxfd = MAXFD
-    return maxfd
-
-
-try:
-    from os import closerange
-except ImportError:
-    def closerange(fd_low, fd_high):    # NOQA
-        # Iterate through and close all file descriptors.
-        for fd in range(fd_low, fd_high):
-            try:
-                os.close(fd)
-            except OSError:    # ERROR, fd wasn't open to begin with (ignored)
-                pass
-
-
-# http://www.svbug.com/documentation/comp.unix.programmer-FAQ/faq_2.html#SEC16
-def daemonize():
-    """Standard daemonization of a process.
-    """
-    # guard to prevent daemonization with gevent loaded
-    for module in sys.modules.keys():
-        if module.startswith('gevent'):
-            raise ValueError('Cannot daemonize if gevent is loaded')
-
-    if hasattr(os, 'fork'):
-        child_pid = os.fork()
-    else:
-        raise ValueError("Daemonizing is not available on this platform.")
-
-    if child_pid != 0:
-        # we're in the parent
-        os._exit(0)
-
-    # child process
-    os.setsid()
-
-    subchild = os.fork()
-    if subchild:
-        os._exit(0)
-
-    # subchild
-    maxfd = get_maxfd()
-    closerange(0, maxfd)
-
-    os.open(REDIRECT_TO, os.O_RDWR)
-    os.dup2(0, 1)
-    os.dup2(0, 2)
-
-
 PROFILE_OPT = click.option('--profile', required=True, default=DEFAULT_PROFILE)
+VERDI_BIN = os.path.abspath(os.path.join(sys.executable, '../verdi'))
+VIRTUALENV = os.path.abspath(os.path.join(sys.executable, '../../'))
 
 
-@click.command('balrog')
+class ProfileConfig(object):
+    _ENDPOINT_TPL = 'tcp://127.0.0.1:{port}'
+
+    def __init__(self, profile):
+        self.profile = profile
+        self.config = get_config()
+        self.profile_config = self.config['profiles'][self.profile]
+        self.profile_config[CIRCUS_PORT_KEY] = self.profile_config.get(CIRCUS_PORT_KEY, generate_new_circus_port(self.profile))
+        update_profile(profile, self.profile_config)
+
+    def get_endpoint(self, port_incr=0):
+        port = self.profile_config[CIRCUS_PORT_KEY]
+        return self._ENDPOINT_TPL.format(port=port + port_incr)
+
+    def get_client(self):
+        return CircusClient(endpoint=self.get_endpoint(), timeout=0.5)
+
+    @property
+    def daemon_name(self):
+        return 'aiida-{}'.format(self.profile)
+
+    @property
+    def cmd_string(self):
+        return '{} -p {} devel run_daemon'.format(VERDI_BIN, self.profile),
+
+@click.group('balrog')
+def balrog():
+    'Manage AiiDA daemons'
+
+
+@balrog.command('quit')
+@PROFILE_OPT
+def quit(profile):
+    profile_config = ProfileConfig(profile)
+    client = profile_config.get_client()
+    client.call({'command': 'quit', 'properties': {}})
+
+
+@balrog.command('status')
+@PROFILE_OPT
+def status(profile):
+    profile_config = ProfileConfig(profile)
+    client = profile_config.get_client()
+    response = {}
+    try:
+        response = client.call({
+            'command': 'status',
+            'properties': {
+                'name': profile_config.daemon_name
+            }
+        })
+    except CallError:
+        pass
+    click.echo(str(response.get('status', 'shut down.')))
+
+
+@balrog.command('start')
 @PROFILE_OPT
 @click.option('--logger-config', 'loggerconfig',
               help=("The location where a standard Python logger configuration INI, "
                     "JSON or YAML file can be found.  This can be used to override "
                     "the default logging configuration for the arbiter.")
               )
-@click.option('--daemon', 'as_daemon', is_flag=True,
+@click.option('--fg', '--foreground', is_flag=True,
               help="Start circusd in the background. Not supported on Windows")
 @click.option('--pidfile')
-@click.option('--version', is_flag=True, default=False, help='Displays Circus version and exits.')
-def main(profile, loggerconfig, as_daemon, pidfile, version):
+def main(profile, loggerconfig, foreground, pidfile):
     """Run an aiida daemon"""
     import zmq
     try:
@@ -106,33 +108,33 @@ def main(profile, loggerconfig, as_daemon, pidfile, version):
         print('circusd needs PyZMQ >= 13.1.0 to run - aborting...')
         sys.exit(0)
 
-    # XXX we should be able to add all these options in the config file as well
     loglevel = 'INFO'
-    logoutput = 'balrog.log'
+    logoutput = '-'
 
-    if version:
-        print(__version__)
-        sys.exit(0)
-
-    if as_daemon:
+    if not foreground:
+        logoutput = 'balrog-{}.log'.format(profile)
         daemonize()
 
-    # From here it can also come from the arbiter configuration
-    # load the arbiter from config
+    # Create the arbiter
+    profile_config = ProfileConfig(profile)
+
     arbiter = get_arbiter(
-        controller='tcp://127.0.0.1:6000',
+        controller=profile_config.get_endpoint(0),
+        pubsub_endpoint=profile_config.get_endpoint(1),
+        stats_endpoint=profile_config.get_endpoint(2),
         logoutput=logoutput,
         loglevel=loglevel,
         debug=False,
         statsd=True,
+        pidfile='balrog-{}.pid'.format(profile),  # aiida.common.setup.AIIDA_CONFIG_FOLDER + '/daemon/aiida-{}.pid'.format(uuid)
         watchers=[{
-            'name': 'aiida',
-            'cmd': '/Users/ricohaeuselmann/.venvwrap/aiida-workflows/bin/verdi devel run_daemon',
-            'virtualenv': '/Users/ricohaeuselmann/.venvwrap/aiida-workflows',
+            'name': profile_config.daemon_name,
+            'cmd': profile_config.cmd_string,
+            'virtualenv': VIRTUALENV,
             'copy_env': True,
             'stdout_stream': {
                 'class': 'FileStream',
-                'filename': 'aiida.log'
+                'filename': '{}.log'.format(profile_config.daemon_name)
             },
             'env': {
                 'PYTHONUNBUFFERED': 'True'
@@ -183,4 +185,4 @@ def main(profile, loggerconfig, as_daemon, pidfile, version):
 
 
 if __name__ == '__main__':
-    main.main()
+    balrog.main()
